9d26b69998ef5fbf336f02f85a421e1edfecc4b9
Alex Gilleran <alex.gilleran@siteminder.com>
2021-04-23 11:18:44 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+
+    res.setHeader("content-type", "application/json");
+    res.writeHead(200);
+
+    const object = { foo: "bar", fizz: "buzz" };
+    res.write("[\n");
+    const rows = 3000;
+    for (let i = 0; i <= rows; i++) {
+      res.write(JSON.stringify(object));
+      if (i < rows) {
+        res.write(",\n");
+      }
+    }
+    res.write("]");
+
+    setTimeout(() => res.end(), 5000);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(8081);
