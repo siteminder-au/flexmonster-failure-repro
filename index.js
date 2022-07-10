e604462b0f0a3f4885215e06d30a01fd29aeb381
Alex Gilleran <alex.gilleran@siteminder.com>
2022-07-11 08:32:14 +1000

    Changed so that it waits 30 minutes and only then sends a response

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("*", async (req, res) => {
   try {
-    res.setHeader("Access-Control-Allow-Origin", "https://fiddle.jshell.net");
+    // res.setHeader("Access-Control-Allow-Origin", "https://fiddle.jshell.net");
 
-    res.setHeader("content-type", "application/json");
-    res.writeHead(200);
+    // res.setHeader("content-type", "application/json");
 
-    const object = { foo: "bar", fizz: "buzz" };
-    res.write("[\n");
-    const rows = 3000;
-    for (let i = 0; i <= rows; i++) {
-      res.write(JSON.stringify(object));
-      if (i < rows) {
-        res.write(",\n");
-      }
-    }
-    res.write("]");
-
-    setTimeout(() => res.end(), 5000);
+    // const object = { foo: "bar", fizz: "buzz" };
+    // res.write("[\n");
+    // const rows = 3000;
+    // for (let i = 0; i <= rows; i++) {
+    //   res.write(JSON.stringify(object));
+    //   if (i < rows) {
+    //     res.write(",\n");
+    //   }
+    // }
+    // res.write("]");
+    console.log(`Called with ${req.path}`)
+    setTimeout(() => res.send({}), 60 * 1000 * 30); // 30 minutes
   } catch (e) {
     console.error(e);
   }
